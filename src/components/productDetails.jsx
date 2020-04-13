2ce173b34c9ddc23cea6267692fb8cd368c80f53
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-13 11:20:30 +0200

    Programmatic Navigation

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    this.props.history.push("/products"); // When go back by browser, push will allow you to go back.
+    this.props.history.replace("/products"); // When go back by browser, replace will not allow you to go back. Can be used if
+    // you want to stop user to go back to login page.
   };
 
   render() {
